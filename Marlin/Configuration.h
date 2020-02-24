cad2995ff09fd17a7869f389afe4ca8d6fdadadd
Fabien Wirig <fabien.wirig@gmail.com>
2020-02-25 00:30:42 +0100

    enhancement

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -809,12 +809,12 @@
 #define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
 
 // Feedrate (mm/m) for the "accurate" probe of each point
-#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
+#define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 4)
 
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
 //   Set to 3 or more for slow probes, averaging the results.
-#define MULTIPLE_PROBING 2
+#define MULTIPLE_PROBING 3
 
 /**
  * Z probes require clearance when deploying, stowing, and moving between
@@ -1065,9 +1065,9 @@
 
   // Set the number of grid points per dimension.
 #if ENABLED (U20_Pro_AutoBed)
-	#define GRID_MAX_POINTS_X 7
+	#define GRID_MAX_POINTS_X 10
 #else
-	#define GRID_MAX_POINTS_X 7
+	#define GRID_MAX_POINTS_X 10
 #endif
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
@@ -1202,7 +1202,7 @@
 
 // Homing speeds (mm/m)
 #define HOMING_FEEDRATE_XY (50*60)
-#define HOMING_FEEDRATE_Z  (20*60)
+#define HOMING_FEEDRATE_Z  (10*60)
 
 // @section calibrate
 
